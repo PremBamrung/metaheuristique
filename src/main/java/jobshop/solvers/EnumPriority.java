b40e290860a8e3bcee142a537c9b4e60e27373d9
PremBamrung <misterpremsimson@hotmail.fr>
2021-03-18 18:22:45 +0100

    deleting enumpriority, adding report

--- src/main/java/jobshop/solvers/EnumPriority.java
+++
@@ -1,8 +0,0 @@
-package jobshop.solvers;
-
-public enum EnumPriority {
-    SPT, //(Shortest Processing Time) : gives priority to the shortest task
-    LRPT, //(Longest Remaining Processing Time) : gives priority to the task belonging to the job with the highest duration
-    EST_SPT, //SPT + restriction to tasks that can start as soon as possible
-    EST_LRPT //EST + restriction to tasks that can start as soon as possible
-}
